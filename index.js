a7bdcdc9844e1398c84479ee2e831d9951dd316e
mdvprasad <prasad.marini@gmail.com>
2020-04-17 12:33:56 +0530

    Added Cors in the index.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 require("./startup/logging")();
 require("./startup/routes")(app);
+require("./startup/cors")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
