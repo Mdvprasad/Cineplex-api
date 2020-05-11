b4e802bd4c8270704f41e465ac8241103eaa6828
mdvprasad <prasad.marini@gmail.com>
2020-05-11 20:27:09 +0530

    changes made to CORS

--- startup/cors.js
+++ startup/cors.js
@@ -2,4 +2,9 @@ const cors = require("cors");
 
 module.exports = function(app) {
   app.use(cors());
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 };
